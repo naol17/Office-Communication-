ca9491c3b31a730ebda7e1427e7454d21bcbd740
naol17 <it.naol.dame12@gmail.com>
2022-12-23 22:44:58 +0300

    finnishing message body

--- office-com/src/Components/Mesage.jsx
+++ office-com/src/Components/Mesage.jsx
@@ -11,7 +11,7 @@ export const Mesage = () => {
         <span>Just now</span>
       </div>
       <div className="messagecontent">
-        <p>Hello</p>
+        <p>Hello hdfdfjhhdf hfdfjdfhdfdhjf dfjd  djf dfjdf jdf djfdfj dfjdfdjf djfdfj dfhdfhdjfdjhfdhfj fjdfkdjfkdfdkfdkfdfkdfdkfdfkdfdkfdkf sdksdksdksdksdksdsk sd skdskd skd skd skd skd skd skd sdk sdk </p>
         <img
           src="https://images.unsplash.com/photo-1671614188183-57bb238ad7c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
           alt=""
--- office-com/src/Components/Navbar.jsx
+++ office-com/src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <div className='navbar'>
 
-      <span className='logo'>Office Communication </span>
+      <span className='logo'>Office-Comm </span>
       <div className='user'>
         <img src='https://images.unsplash.com/photo-1671614188183-57bb238ad7c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60' alt=''></img>
         <span>Naol</span>

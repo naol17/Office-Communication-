e532b2a058954fea6ff0fb3d846bdf004630b1ca
naol17 <it.naol.dame12@gmail.com>
2022-12-23 17:59:27 +0300

    edditing message body

--- office-com/src/Components/Mesage.jsx
+++ office-com/src/Components/Mesage.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export const Mesage = () => {
   return (
-    <div className='message'>
+    <div className="message">
       <div className="mesageinfo">
-        
+        <img
+          src="https://images.unsplash.com/photo-1671614188183-57bb238ad7c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <span>Just now</span>
       </div>
-      
+      <div className="messagecontent">
+        <p>Hello</p>
+        <img
+          src="https://images.unsplash.com/photo-1671614188183-57bb238ad7c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+      </div>
     </div>
-  )
-}
-
+  );
+};

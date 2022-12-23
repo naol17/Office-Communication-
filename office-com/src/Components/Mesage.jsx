f832bd422869978e28fe33b24ae9c4f1ac1c51dc
naol17 <it.naol.dame12@gmail.com>
2022-12-23 18:21:13 +0300

    message sender side editing

--- office-com/src/Components/Mesage.jsx
+++ office-com/src/Components/Mesage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Mesage = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="mesageinfo">
         <img
           src="https://images.unsplash.com/photo-1671614188183-57bb238ad7c7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"

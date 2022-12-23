98706e1d5787017b7dbefc2c49af4967dda2ce2f
naol17 <it.naol.dame12@gmail.com>
2022-12-23 23:59:35 +0300

    handling registration form

--- office-com/src/pages/Register.jsx
+++ office-com/src/pages/Register.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import  { BsFillImageFill } from "react-icons/bs";
-
-
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {auth} from '../firebase';
 export const Register = () => {
+
+
+  const handlesubmit = (e)=>{
+    e.preventDefault()
+    const Name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+  }
+  // const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
   return (
     <div className='formcontainer'>
         <div className='firmwrapper'>
           <span className='logo'>Office Communication</span>
           <span className='title'>Register</span>
-          <form>
+          <form onSubmit={handlesubmit}>
             <input type='text' placeholder="Name"/>
             <input type='email' placeholder="email"/>
             <input type='password' placeholder="password"/>

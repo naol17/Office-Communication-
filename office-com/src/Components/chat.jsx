ee2dfe0c60906a89c1bb012852392fc086cc7627
naol17 <it.naol.dame12@gmail.com>
2023-01-03 14:07:15 +0300

    re creating chat and chats component

--- office-com/src/Components/chat.jsx
+++ office-com/src/Components/chat.jsx
@@ -1,25 +1,60 @@
-import React from 'react'
+import React, { useContext } from "react";
 import {HiUserAdd} from 'react-icons/hi'
 import{FiMoreVertical} from 'react-icons/fi'
-import { Input } from './Input'
-import {Messages} from './Messages'
-import { useContext } from 'react'
-// import { AuthContext } from '../context/AuthContext'
-import { ChatContext } from '../context/ChatContext'
+
+import { ChatContext } from "../context/ChatContext";
+import { Messages } from "./Messages";
+import { Input } from "./Input";
+
 export const Chat = () => {
-  const[data] = useContext(ChatContext)
+  const { data } = useContext(ChatContext);
+
   return (
-    <div className='chat'>
+    <div className="chat">
       <div className="chatinfo">
-      <span>{data.user?.displayName}</span>
-          <div className="chaticons">
-        <HiUserAdd style={{color: 'red', fontSize: '20px'}} /> <FiMoreVertical  style={{color: 'red', fontSize: '20px'}}/>
+        <span>{data.user?.displayName}</span>
+        <div className="chaticons">
+       <HiUserAdd style={{color: 'red', fontSize: '20px'}} /> <FiMoreVertical  style={{color: 'red', fontSize: '20px'}}/>
+
         </div>
       </div>
-      <Messages/>
-      <Input/>
-
+     <Messages/>
+     <Input/>
     </div>
-  )
-}
+  );
+};
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import {HiUserAdd} from 'react-icons/hi'
+// import{FiMoreVertical} from 'react-icons/fi'
+// import { Input } from './Input'
+// import {Messages} from './Messages'
+// import { useContext } from 'react'
+// // import { AuthContext } from '../context/AuthContext'
+// import { ChatContext } from '../context/ChatContext'
+// export const Chat = () => {
+//   const[data] = useContext(ChatContext)
+//   return (
+//     <div className='chat'>
+//       <div className="chatinfo">
+//       <span>{data.user?.displayName}</span>
+//           <div className="chaticons">
+//         <HiUserAdd style={{color: 'red', fontSize: '20px'}} /> <FiMoreVertical  style={{color: 'red', fontSize: '20px'}}/>
+//         </div>
+//       </div>
+//       <Messages/>
+//       <Input/>
+
+//     </div>
+//   )
+// }
 

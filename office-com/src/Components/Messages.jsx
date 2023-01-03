70d660aa6a54b473ea6be59a2e7dc2dfb0d02bea
naol17 <it.naol.dame12@gmail.com>
2023-01-03 14:09:43 +0300

    update message and messages components

--- office-com/src/Components/Messages.jsx
+++ office-com/src/Components/Messages.jsx
@@ -1,13 +1,8 @@
 import { doc, onSnapshot } from "firebase/firestore";
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ChatContext } from "../context/ChatContext";
 import { db } from "../firebase";
-import { Input } from "./Input";
-import { Mesage } from "./Mesage";
-
+import {Mesage} from "./Mesage"
 export const Messages = () => {
   const [messages, setMessages] = useState([]);
   const { data } = useContext(ChatContext);
@@ -33,4 +28,44 @@ export const Messages = () => {
   );
 };
 
-// export default Messages;
+
+
+
+
+// import { doc, onSnapshot } from "firebase/firestore";
+// import React from "react";
+// import { useState } from "react";
+// import { useEffect } from "react";
+// import { useContext } from "react";
+// import { ChatContext } from "../context/ChatContext";
+// import { db } from "../firebase";
+// import { Input } from "./Input";
+// import { Mesage } from "./Mesage";
+
+// export const Messages = () => {
+//   const [messages, setMessages] = useState([]);
+//   const { data } = useContext(ChatContext);
+
+//   useEffect(() => {
+//     const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+//       doc.exists() && setMessages(doc.data().messages);
+//     });
+
+//     return () => {
+//       unSub();
+//     };
+//   }, [data.chatId]);
+
+//   console.log(messages)
+
+//   return (
+//     <div className="messages">
+//       {messages.map((m) => (
+//         <Mesage message={m} key={m.id} />
+//       ))}
+//       <Input/>
+//     </div>
+//   );
+// };
+
+// // export default Messages;

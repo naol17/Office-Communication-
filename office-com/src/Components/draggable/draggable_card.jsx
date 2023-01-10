fd87883500bf6b1de4134fa83949277c19bb7dbd
naol17 <it.naol.dame12@gmail.com>
2023-01-10 23:48:24 +0300

    creating draggable states

--- office-com/src/Components/draggable/draggable_card.jsx
+++ office-com/src/Components/draggable/draggable_card.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import Draggable from "react-draggable";
 
-export const Draggable_card = () => {
-  return <div>draggable_card</div>;
+export const Draggable_Card = () => {
+  // const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  // const trackPos = (data) => {
+  //   setPosition({ x: data.x, y: data.y });
+  // };
+
+  const [tasks, SetTasks] = useState([]);
+  const [additems, SetAdditems] = useState(false);
+
+  const handleSubmit = () => {
+    SetAdditems(!additems);
+  };
+
+  const addTask = (task) => {
+    SetTasks(task);
+  };
+
+  return (
+    <div>
+      {/* <Draggable onDrag={(e, data) => trackPos(data)}>
+        <div className="box">
+          <div>Move me around!</div>
+          <div>
+            x: {position.x.toFixed(0)}, y: {position.y.toFixed(0)}
+          </div>
+        </div>
+      </Draggable> */}
+    </div>
+  );
 };
